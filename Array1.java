405d5081689993ff4804d530d4f356203826423e
Ana Borges <78245890+aborgees@users.noreply.github.com>
2021-02-10 17:27:52 -0300

    Create Array1.java

---
+++ Array1.java
@@ -0,0 +1,27 @@
+public class Array1 {
+public static void main(String[] args) {
+
+		Integer array[] = new Integer[9];
+		int contador1 = 0;
+
+		for ( contador1 = 0; contador1 < array.length; contador1++ )	{
+			array[contador1] = contador1;
+		}
+
+		System.out.println("O elemento do array eh: ");
+
+		for ( contador1 = 0; contador1< array.length; contador1++ )	{
+			System.out.println( array[contador1] );
+		}
+
+		for ( contador1 = 0; contador1 < array.length; contador1++ )	{
+			array[contador1] = array[contador1] * 3;
+		}
+
+		System.out.println("O elemento do array multiplicado por 3 eh igual: ");
+
+		for ( contador1 = 0; contador1 < array.length; contador1++ )	{
+			System.out.println( array[contador1] );
+		}
+	}
+}
